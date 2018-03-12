ea3f873175af401a9efceab80819905ad38e464d
IAN-PC\IAN <sebastianmendez1323@gmail.com>
2018-03-11 19:44:11 -0500

    *Nuevo archivo del proyecto

---
+++ ProyectoUnity/Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayerControler : MonoBehaviour {
+
+	//Aplicar fuerza a la esfera para que se mueva, componente caracteristica solides afectado por la gravedad
+
+	public float speed;
+	private Rigidbody rb;
+	public Transform particulas;
+	private ParticleSystem systemaParticulas;
+	private Vector3 posicion;
+	private AudioSource audioRecoleccion;
+	private int cubos = 12;
+	private int contador;
+	public Text TextoContador;
+	public Text TextoFin;
+
+	// Use this for initialization
+	void Start () {
+		TextoContador.text = "Contador: " + contador.ToString();
+		TextoContador.text = "" ;
+		contador = 0;
+		rb = GetComponent<Rigidbody> ();
+		systemaParticulas = particulas.GetComponent<ParticleSystem> ();
+		systemaParticulas.Stop ();
+		audioRecoleccion = GetComponent<AudioSource>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void FixedUpdate(){
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		//float moveHorizontal = Input.acceleration.x;(Movil)
+		//float moveVertical = Input.acceleration.z;(Movil)
+		Vector3 movimiento = new Vector3 (moveHorizontal,0.0f, moveVertical);
+		//Vector3 movimiento = new Vector3 (moveHorizontal,0.0f, -moveVertical);(Movil)
+		rb.AddForce (movimiento*speed);
+	}
+
+	//referencia los objetos que impactamos
+	void OnTriggerEnter(Collider other ){
+		if (other.gameObject.CompareTag ("Recolectable")) {
+			contador = contador + 1;
+			TextoContador.text = "Contador: " + contador.ToString();
+			cubos = cubos - 1;
+			other.gameObject.SetActive (false);
+			posicion = other.gameObject.transform.position;
+			particulas.position = posicion;
+			systemaParticulas = particulas.GetComponent<ParticleSystem> ();
+			systemaParticulas.Play ();
+			audioRecoleccion.Play ();
+			Debug.Log(cubos);
+			if (cubos == 0) {
+				//Debug.Log("Fin del nivel");
+				//SceneManager.LoadScene (1);
+				TextoFin.text = "Fin del juego, has ganado";
+			}
+
+		}
+
+		//Desaparece el objeto
+		//Destroy (other.gameObject);
+
+	}
+}
---
+++ ProyectoUnity/Assets/Scripts/Salir.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Salir : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void SalirAplicacion(){
+		Debug.Log ("Saliendo de la app");
+		Application.Quit ();
+	}   
+
+
+}
