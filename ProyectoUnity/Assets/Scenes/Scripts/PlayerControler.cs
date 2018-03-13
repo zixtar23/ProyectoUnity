ba0597ac16b2c5dba86bedd668e47389d9974d7b
IAN-PC\IAN <sebastianmendez1323@gmail.com>
2018-03-13 16:52:01 -0500

    *JuegoUnity sin tiempo

--- ProyectoUnity/Assets/Scenes/Scripts/PlayerControler.cs
+++ ProyectoUnity/Assets/Scenes/Scripts/PlayerControler.cs
@@ -18,24 +18,29 @@ public class PlayerControler : MonoBehaviour {
     private int cuboObjetivo = 0;
 	private int contador;
 	public Text TextoContador;
-	public Text TextoFin;
+    public Button Nivel2btn;
+    private GameObject[] gameObjects;
 
-	// Use this for initialization
-	void Start () {
+
+    // Use this for initialization
+    void Start () {
+        //TextoFin.text = "";
 		TextoContador.text = "Bonus: " + contador.ToString();
-		TextoContador.text = "" ;
+		//TextoContador.text = "" ;
 		contador = 0;
 		rb = GetComponent<Rigidbody> ();
 		systemaParticulas = particulas.GetComponent<ParticleSystem> ();
 		systemaParticulas.Stop ();
 		audioRecoleccion = GetComponent<AudioSource>();
-		
-	}
+        Nivel2btn = GameObject.Find("Nivel2").GetComponent<Button>();
+        Nivel2btn.gameObject.SetActive(false);
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        
+    }
 	void FixedUpdate(){
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
@@ -47,26 +52,25 @@ public class PlayerControler : MonoBehaviour {
 	}
 
 	//referencia los objetos que impactamos
-	void OnTriggerEnter(Collider other ){
-		if (other.gameObject.CompareTag ("Recolectable")) {
+	void OnTriggerEnter(Collider other ){        
+        if (other.gameObject.CompareTag ("Recolectable")) {
 			contador = contador + 1;
 			TextoContador.text = "Bonus: " + contador.ToString();
+            Debug.Log(contador);
 			cubos = cubos + 1;
 			other.gameObject.SetActive (false);
 			posicion = other.gameObject.transform.position;
-			particulas.position = posicion;
-			systemaParticulas = particulas.GetComponent<ParticleSystem> ();
-			systemaParticulas.Play ();
-			audioRecoleccion.Play ();
-			Debug.Log(cubos);
-			if (cubos == 3) {
-				//Debug.Log("Fin del nivel");
-				//SceneManager.LoadScene (1);
-				TextoFin.text = "Fin del nivel";
-
-			}
-
-		}
+			particulas.position = posicion;            
+            systemaParticulas = particulas.GetComponent<ParticleSystem> ();
+            systemaParticulas.Play();
+            Invoke("StopEmitter",1);
+            audioRecoleccion.Play ();
+            //Debug.Log(cubos);
+            //	if (cubos == 3) {
+            //Debug.Log("Fin del nivel");
+            //SceneManager.LoadScene (1);	
+            //}
+        }
 
         if (other.gameObject.CompareTag("Final")){
 			cuboObjetivo = cuboObjetivo + 1;
@@ -74,13 +78,36 @@ public class PlayerControler : MonoBehaviour {
 			posicion = other.gameObject.transform.position;
 			particulas.position = posicion;
 			systemaParticulas = particulas.GetComponent<ParticleSystem> ();
-			if (cuboObjetivo == 1)
+            systemaParticulas.Play();
+            audioRecoleccion.Play();
+            if (cuboObjetivo == 1)
+            {               
+                Nivel2btn.gameObject.SetActive(true);
+                //TextoFin.text = "Siguiente nivel?";
+            }
+
+        }
+
+        if (other.gameObject.CompareTag("Plataforma")){
+            Debug.Log("collision name = " + other.gameObject.name);
+            if (other.gameObject.name == "Ground")
             {
-                SceneManager.LoadScene(1);
+                gameObjects = GameObject.FindGameObjectsWithTag("planos");
+
+                for (var i = 0; i < gameObjects.Length; i++)
+                {
+                    Destroy(gameObjects[i]);
+                }
+                //other.gameObject.SetActive(false);
+
             }
         }
             //Desaparece el objeto
             //Destroy (other.gameObject);
 
         }
-}
+    private void StopEmitter()
+    {
+        systemaParticulas.Stop();
+    }
+    }
