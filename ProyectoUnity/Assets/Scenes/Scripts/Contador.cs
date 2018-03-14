a3502cb35bb7202b7a867a4aa74c568d1f0f3085
IAN-PC\IAN <sebastianmendez1323@gmail.com>
2018-03-14 09:26:58 -0500

    *Contador añadido. *Correcion de errores menores.

--- ProyectoUnity/Assets/Scenes/Scripts/Contador.cs
+++ ProyectoUnity/Assets/Scenes/Scripts/Contador.cs
@@ -4,19 +4,21 @@ using UnityEngine;
 using UnityEngine.UI;
 public class Contador : MonoBehaviour {
 
-    public Text  TiempoText;
-    public float TiempoStamp;
+    public Text  contadorTexto;
+    public float segundos, minutos;
     public bool UsarContador = false;
 
 
 
 	// Use this for initialization
 	void Start () {
-		
+        contadorTexto = GetComponent<Text>() as Text;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        minutos = (int)(Time.time / 60f);
+        segundos = (int)(Time.time % 60f);
+        contadorTexto.text ="Tiempo: " +  minutos.ToString("00") + ":" + segundos.ToString("00");
+    }
 }
